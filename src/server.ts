459253c061a53beb773eddb5262be4762eeddf05
thisisroni <thisisroni@gapp.nthu.edu.tw>
2026-05-20 01:41:22 +0800

    prettier error

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,9 @@ import { buildApp } from './app';
 
 const app = buildApp({ logger: true });
 const port = Number(process.env.PORT || 3000);
-const host = process.env.HOST || '0.0.0.0';
+const host=process.env.HOST || '0.0.0.0';
 
-async function start() {
+async function start(){
   try {
     await app.listen({ port, host });
     app.log.info(`Server listening at http://${host}:${port}`);
